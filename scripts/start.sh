076f2f92d1c981dfd5b811bd7c71188106ece4e8
ayaz-125 <ayazr425@gmail.com>
2025-04-08 03:20:03 +0530

    add

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-cd /home/ec2-user/myapp
+
+echo "Running start.sh..." >> /home/ec2-user/start.log
+
+cd /home/ec2-user/myapp || {
+  echo "Failed to cd into /home/ec2-user/myapp" >> /home/ec2-user/start.log
+  exit 1
+}
+
 nohup python3 app.py > app.log 2>&1 &
+
+if [ $? -eq 0 ]; then
+  echo "Flask app started successfully." >> /home/ec2-user/start.log
+else
+  echo "Failed to start Flask app." >> /home/ec2-user/start.log
+  exit 1
+fi
