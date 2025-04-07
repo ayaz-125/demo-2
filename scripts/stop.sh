cfcf022e246880badac5352c6cba35842cb635bd
ayaz-125 <ayazr425@gmail.com>
2025-04-08 03:07:31 +0530

    add

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-pkill -f app.py || true
+echo "Running stop.sh..." >> /home/ec2-user/stop.log
+
+# Kill app.py if it matches the full path
+pkill -f "python3 app.py" || pkill -f "python3 my_app/app.py" || echo "No app.py process found" >> /home/ec2-user/stop.log
+
+echo "stop.sh completed." >> /home/ec2-user/stop.log
+exit 0
