474c31425e62d5f6e9cd61cfc06bde9853e07c74
ayaz-125 <ayazr425@gmail.com>
2025-04-08 04:08:01 +0530

    aa

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Running start.sh..." >> /home/ec2-user/start.log
+echo "Running start.sh..." >> /home/ubuntu/start.log
 
-cd /home/ec2-user/my_app || {
-  echo "Failed to cd into /home/ec2-user/my_app" >> /home/ec2-user/start.log
+cd /home/ubuntu/my_app || {
+  echo "Failed to cd into /home/ubuntu/my_app" >> /home/ubuntu/start.log
   exit 1
 }
 
 nohup python3 app.py > app.log 2>&1 &
 
 if [ $? -eq 0 ]; then
-  echo "Flask app started successfully." >> /home/ec2-user/start.log
+  echo "Flask app started successfully." >> /home/ubuntu/start.log
 else
-  echo "Failed to start Flask app." >> /home/ec2-user/start.log
+  echo "Failed to start Flask app." >> /home/ubuntu/start.log
   exit 1
 fi
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo "Running stop.sh..." >> /home/ec2-user/stop.log
-
-# Kill app.py if it matches the full path
-pkill -f "python3 app.py" || pkill -f "python3 my_app/app.py" || echo "No app.py process found" >> /home/ec2-user/stop.log
-
-echo "stop.sh completed." >> /home/ec2-user/stop.log
+echo "Running stop.sh..." >> /home/ubuntu/stop.log
+pkill -f app.py || echo "No app.py process found" >> /home/ubuntu/stop.log
+echo "stop.sh completed." >> /home/ubuntu/stop.log
 exit 0
